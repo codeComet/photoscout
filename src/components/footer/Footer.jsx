1981f61e7c670c9bd15bd00828aca032af78f7aa
codeComet <unknownmind10@gmail.com>
2025-03-11 12:04:53 +0600

    :speech_balloon: Footer text added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,19 @@ const Footer = () => {
     <footer className="bg-gray-900 text-gray-400 py-4 sm:py-6">
       <Newsletter />
       <div className="max-w-7xl mx-auto pt-3 sm:pt-5 px-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 sm:gap-0">
-        <div className="text-lg sm:text-xl font-semibold text-white">PhotoScout</div>
+        <div className="text-lg sm:text-xl font-semibold text-white">
+          PhotoScout
+        </div>
+        <div className="text-xs sm:text-sm">
+          Made with ❤️ by{" "}
+          <a
+            href="https://devbishal.netlify.app"
+            target="_blank"
+            className="underline text-[#16A44B]"
+          >
+            Bishal
+          </a>
+        </div>
         <div className="text-xs sm:text-sm text-center">
           © {new Date().getFullYear()} PhotoScout. All rights reserved.
         </div>
